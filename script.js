5685a4b20f2b411d8881f23c4c37a91d516db4fd
Sedaezt <sedaezzat@icloud.com>
2024-05-17 19:25:33 +0200

    score bijhouden

--- script.js
+++ script.js
@@ -1,43 +1,69 @@
 const monumentImage = document.getElementById('monument-image');
-const leftOption = document.getElementById('left-option');
-const rightOption = document.getElementById('right-option');
+const option1 = document.getElementById('option1');
+const option2 = document.getElementById('option2');
+const scoreDisplay = document.getElementById('score');  // score bijhouden
 
 const monuments = [
-    { image: 'atomium.jpeg', correctAnswer: 'left' },
-    { image: 'eiffeltoren.jpeg', correctAnswer: 'right' },
+    { image: 'atomium.jpeg', countries: ['België', 'Nederland'], correctAnswer: 0 },
+    { image: 'eiffeltoren.jpeg', countries: ['Frankrijk', 'Spanje'], correctAnswer: 0 },
 ];
 
-// monument index
-let currentMonumentIndex = 0;  
+let currentMonumentIndex = 0;
 let xOrientation = null;
+let tiltThreshold = 15;
+let score = 0;
 
+//apparaat oriëntatie veranderen
+window.addEventListener('deviceorientation', handleOrientation);
+
+// functie aanroepen
+function handleOrientation(event) {
+    if (event.beta !== null) {
+        xOrientation = event.beta;
+    }
+
+    if (xOrientation !== null) {
+        if (xOrientation > tiltThreshold) {
+            checkAnswer(0); // Optie 1
+        } else if (xOrientation < -tiltThreshold) {
+            checkAnswer(1); // Optie 2
+        }
+    }
+}
 
 // monument tonen
 function showMonument() {
+    // uit array halen
     const currentMonument = monuments[currentMonumentIndex];
     monumentImage.src = currentMonument.image;
 
-    // klikken op knop
-    leftOption.addEventListener('click', () => checkAnswer('left'));
-    rightOption.addEventListener('click', () => checkAnswer('right'));
+    // tekst veranderen
+    option1.textContent = currentMonument.countries[0];
+    option2.textContent = currentMonument.countries[1];
+
+    updateScore();
 }
 
-// checken of het klopt
+// controleren of het geselecteerde antwoord correct is
 function checkAnswer(selectedOption) {
     const currentMonument = monuments[currentMonumentIndex];
     if (selectedOption === currentMonument.correctAnswer) {
-        alert('Juist!');
-    } else {
-        alert('Fout!');
+        score++;
+        alert('Correct!');
     }
 
+    // naar het volgende monument gaan
     currentMonumentIndex++;
-    // opnieuw beginnen
     if (currentMonumentIndex >= monuments.length) {
         currentMonumentIndex = 0;
     }
 
     showMonument();
+}
+
+// score weergevengit
+function updateScore() {
+    scoreDisplay.textContent = 'Score: ' + score;
 }
 
 showMonument();
